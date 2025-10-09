d9f6ec5847f2698cb272377b36318f2539a94d4d
aashikhameed <aashikhameed@gmail.com>
2025-10-09 15:14:04 +0530

    formatted code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,93 +21,94 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-val signingKeystorePath = System.getenv("KEYSTORE_PATH") ?: throw GradleException("KEYSTORE_PATH not set")
+    
+    val signingKeystorePath = System.getenv("KEYSTORE_PATH") ?: throw GradleException("KEYSTORE_PATH not set")
     val signingKeystorePassword = System.getenv("KEYSTORE_PASSWORD") ?: throw GradleException("KEYSTORE_PASSWORD not set")
-        val signingKeyAlias = System.getenv("KEY_ALIAS") ?: throw GradleException("KEY_ALIAS not set")
-            val signingKeyPassword = System.getenv("KEY_PASSWORD") ?: throw GradleException("KEY_PASSWORD not set")
+    val signingKeyAlias = System.getenv("KEY_ALIAS") ?: throw GradleException("KEY_ALIAS not set")
+    val signingKeyPassword = System.getenv("KEY_PASSWORD") ?: throw GradleException("KEY_PASSWORD not set")
 
 
-                signingConfigs {
-                            create("release") {
-                                            storeFile = file(signingKeystorePath)
-                                                        storePassword = signingKeystorePassword
-                                                                    keyAlias = signingKeyAlias
-                                                                                keyPassword = signingKeyPassword
-                            }
-                }
+    signingConfigs {
+        create("release") {
+            storeFile = file(signingKeystorePath)
+            storePassword = signingKeystorePassword
+            keyAlias = signingKeyAlias
+            keyPassword = signingKeyPassword
+        }
+    }
 
-                    buildTypes {
-                                release {
-                                                isMinifyEnabled = true
-                                                            isShrinkResources = true
-                                                                        signingConfig = signingConfigs.getByName("release")
-                                                                                    proguardFiles(
-                                                                                                        getDefaultProguardFile("proguard-android-optimize.txt"),
-                                                                                                                        "proguard-rules.pro"
-                                                                                    )
-                                }
-                    }
-                        compileOptions {
-                                    sourceCompatibility = JavaVersion.VERSION_1_8
-                                            targetCompatibility = JavaVersion.VERSION_1_8
-                        }
-                            kotlinOptions {
-                                        jvmTarget = "1.8"
-                            }
+    buildTypes {
+    release {
+        isMinifyEnabled = true
+        isShrinkResources = true
+        signingConfig = signingConfigs.getByName("release")
+        proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+        )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 
-                                buildFeatures {
-                                            compose = true
-                                }
-                                    composeOptions {
-                                                kotlinCompilerExtensionVersion = "1.5.1"
-                                    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
 
-                                        packaging {
-                                                    resources {
-                                                                    excludes += "/META-INF/{AL2.0,LGPL2.1}"
-                                                    }
-                                        }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 
 }
 
 dependencies {
-        implementation(libs.androidx.core.ktx)
-            implementation(libs.androidx.appcompat)
-                implementation(libs.material)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
 
-                    // Jetpack Compose & ViewModel
-                        implementation(platform(libs.androidx.compose.bom))
-                            implementation(libs.androidx.compose.material3)
-                                implementation(libs.androidx.compose.ui.tooling.preview)
-                                    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    // Jetpack Compose & ViewModel
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
-                                        // Room DB
-                                            implementation(libs.androidx.room.runtime)
-                                                ksp(libs.androidx.room.compiler)
-                                                    implementation(libs.androidx.room.ktx)
+    // Room DB
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
 
-                                                        // DataStore
-                                                            implementation(libs.androidx.datastore.preferences)
+    // DataStore
+    implementation(libs.androidx.datastore.preferences)
 
-                                                                // ExoPlayer
-                                                                    implementation("androidx.media3:media3-exoplayer:1.3.1")
-                                                                        implementation("androidx.media3:media3-ui:1.3.1")
-                                                                            implementation("androidx.compose.animation:animation")
-                                                                                implementation("androidx.compose.animation:animation-core")
-                                                                                    implementation("io.coil-kt:coil-compose:2.5.0")
-                                                                                        implementation("io.coil-kt:coil-compose:2.7.0")
-                                                                                            // Permissions
-                                                                                                implementation("com.google.accompanist:accompanist-permissions:0.32.0")
-                                                                                                    implementation("androidx.compose.material:material-icons-extended")
-                                                                                                        implementation("io.coil-kt:coil-compose:2.5.0")
-                                                                                                            implementation ("androidx.media:media:1.6.0")
+    // ExoPlayer
+    implementation("androidx.media3:media3-exoplayer:1.3.1")
+    implementation("androidx.media3:media3-ui:1.3.1")
+    implementation("androidx.compose.animation:animation")
+    implementation("androidx.compose.animation:animation-core")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
+    // Permissions
+    implementation("com.google.accompanist:accompanist-permissions:0.32.0")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation ("androidx.media:media:1.6.0")
 
 
-                                                                                                                // Testing
-                                                                                                                    testImplementation(libs.junit)
-                                                                                                                        androidTestImplementation(libs.androidx.junit)
-                                                                                                                            androidTestImplementation(libs.androidx.espresso.core)
-                                                                                                                                implementation(libs.androidx.activity.compose)
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.activity.compose)
 
 }
 
@@ -115,9 +116,9 @@ fun gradleLocalProperties(projectRootDir: File): Properties {
         val properties = Properties()
             val localPropertiesFile = File(projectRootDir, "local.properties")
                 if (localPropertiesFile.exists()) {
-                            localPropertiesFile.inputStream().use { input ->
-                                        properties.load(input)
-                                                }
+                    localPropertiesFile.inputStream().use { input ->
+                        properties.load(input)
+                    }
                 }
-                    return properties
+        return properties
 }
